35128e8c65739c4127e4399aa1b8a386aaa5efe5
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-25 21:33:16 -0500

    added clock functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
  //get current time to display on page 
- let currentTime = moment().format("MMM Do, YYYY hh:mm:ss");
- console.log(currentTime);
+ let now = moment().format("MMMM Do, YYYY hh:mm:ss");
+ console.log(now);
+let currentDay = document.getElementById('currentDay');
+currentDay.innerHTML = now;
