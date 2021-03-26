4693ded61294a7df444b884840c760b99eccde13
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-25 22:05:50 -0500

    updated clock

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
- //get current time to display on page 
- let now = moment().format("MMMM Do, YYYY hh:mm:ss");
- console.log(now);
-let currentDay = document.getElementById('currentDay');
-currentDay.innerHTML = now;
+//get current time to display on page 
+function printTime(){
+    let currentDay = document.getElementById('currentDay');
+    let now = moment().format("MMMM Do, YYYY hh:mm:ss");
+    currentDay.textContent = now;
+    }   
+   setInterval(printTime, 1000);
